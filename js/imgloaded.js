23c0a51b60973b88910c99d02c043a35e4fbfab2
xingg01 <128922195+xingg01@users.noreply.github.com>
2026-01-23 15:34:11 +0800

    部署 Hexo 博客，更新于 $(date '+%Y-%m-%d %H:%M:%S')

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -80,8 +80,8 @@ const executeLoad = (config, target) => {
 const config = {
   smallSrc: '/img/top_small.png', // 小图链接 尽可能配置小于100k的图片
   largeSrc: '/img/top.png', // 大图链接 最终显示的图片
-  mobileSmallSrc: '/img/sjxt.jpg', // 手机端小图链接 尽可能配置小于100k的图片
-  mobileLargeSrc: '/img/sjdt.jpg', // 手机端大图链接 最终显示的图片
+  mobileSmallSrc: '/img/top_small.png', // 手机端小图链接 尽可能配置小于100k的图片
+  mobileLargeSrc: '/img/top.png', // 手机端大图链接 最终显示的图片
   enableRoutes: ['/'],
   };
 
